5ee706f006374c8b1ba4ee598c689872efcf9091
Faisal Mohammed <m.faisal244@gmail.com>
2022-08-15 22:58:42 +0100

    Completed Navbar component
    - Created Hamburger menu and slide in and out animations using Framer
    - Created and styled navigation links
    - Added a placeholder logo
    - Added icons to the assets folder, created images.js in the constants folder, this file imports and exports all images so they can be imported with ease where ever required by typing `import { images } from "../../constants";`
    - All navbar styling can be found in Navbar.scss

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from "react";
 
 import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
 import { Navbar } from "./components";
+import "./App.scss";
 
 const App = () => {
 	return (
-		<div>
+		<div className="app">
 			<Navbar />
 			<Header />
 			<About />
