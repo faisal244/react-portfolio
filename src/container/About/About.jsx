9558b36d148539da2879d095ec666b8501683e30
Faisal Mohammed <m.faisal244@gmail.com>
2022-08-24 17:02:54 +0100

    Added work history to skills & experience section
    - Got job history data stored in the sanity CMS backend to render on the front end in the skills section
    
    - Used the react tooltip package so that when a user hovers over a job, a tooltip pops up proving more detail about my time in that role
    
    - Used AppWrap and MotionWrap in the export statements for the about and skills container so that these sections will re-animate if the user scrolls back to them, previously they would only animate once on load. Also used this to overwrite the background colour for certain sections, os the background alternates between blue and white as the user navigates through each section
    
    - Added a grey background colour to each skills icon so they contrast better against the white background

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,68 +1,49 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import "./About.scss";
-import { AppWrap } from "../../wrapper";
+import { AppWrap, MotionWrap } from "../../wrapper";
 import { urlFor, client } from "../../client";
 
-// const abouts = [
-//   {
-//     title: "Front End Development",
-//     description: "i am a good web developer",
-//     imgUrl: images.about01,
-//   },
-//   {
-//     title: "Backend Development",
-//     description: "i am a good web developer",
-//     imgUrl: images.about02,
-//   },
-//   {
-//     title: "Full Stack Development",
-//     description: "i am a good web developer",
-//     imgUrl: images.about04,
-//   },
-//   {
-//     title: "UI/UX",
-//     description: "i am a good web developer",
-//     imgUrl: images.about03,
-//   },
-// ];
-
 const About = () => {
-  const [abouts, setAbouts] = useState([]);
+	const [abouts, setAbouts] = useState([]);
 
-  useEffect(() => {
-    const query = '*[_type=="abouts"]';
+	useEffect(() => {
+		const query = '*[_type=="abouts"]';
 
-    client.fetch(query).then((data) => setAbouts(data));
-  }, []);
-  return (
-    <>
-      <h2 className="head-text">
-        I know that <span>Good Development</span> <br /> means{" "}
-        <span>Good Business</span>
-      </h2>
+		client.fetch(query).then((data) => setAbouts(data));
+	}, []);
+	return (
+		<>
+			<h2 className="head-text">
+				I know that <span>Good Development</span> <br /> means{" "}
+				<span>Good Business</span>
+			</h2>
 
-      <div className="app__profiles">
-        {abouts.map((about, index) => (
-          <motion.div
-            whileInView={{ opacity: 1 }}
-            whileHover={{ scale: 1.1 }}
-            transition={{ duration: 0.5, type: "tween" }}
-            className="app__profile-item"
-            key={about.title + index}
-          >
-            <img src={urlFor(about.imgUrl)} alt={about.title} />
-            <h2 className="bold-text" style={{ marginTop: 20 }}>
-              {about.title}
-            </h2>
-            <p className="p-text" style={{ marginTop: 10 }}>
-              {about.description}
-            </p>
-          </motion.div>
-        ))}
-      </div>
-    </>
-  );
+			<div className="app__profiles">
+				{abouts.map((about, index) => (
+					<motion.div
+						whileInView={{ opacity: 1 }}
+						whileHover={{ scale: 1.1 }}
+						transition={{ duration: 0.5, type: "tween" }}
+						className="app__profile-item"
+						key={about.title + index}
+					>
+						<img src={urlFor(about.imgUrl)} alt={about.title} />
+						<h2 className="bold-text" style={{ marginTop: 20 }}>
+							{about.title}
+						</h2>
+						<p className="p-text" style={{ marginTop: 10 }}>
+							{about.description}
+						</p>
+					</motion.div>
+				))}
+			</div>
+		</>
+	);
 };
 
-export default AppWrap(About, "about");
+export default AppWrap(
+	MotionWrap(About, "app__about"),
+	"about",
+	"app__whitebg"
+);
--- src/container/Skills/Skills.jsx
+++ src/container/Skills/Skills.jsx
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
-import reactToolTip from "react-tooltip";
+import ReactTooltip from "react-tooltip";
 
 import { AppWrap, MotionWrap } from "../../wrapper";
 import { urlFor, client } from "../../client";
-
 import "./Skills.scss";
 
 const Skills = () => {
-	const [experience, setExperience] = useState([]);
+	const [experiences, setExperiences] = useState([]);
 	const [skills, setSkills] = useState([]);
 
 	useEffect(() => {
@@ -16,7 +15,7 @@ const Skills = () => {
 		const skillsQuery = '*[_type == "skills"]';
 
 		client.fetch(query).then((data) => {
-			setExperience(data);
+			setExperiences(data);
 		});
 
 		client.fetch(skillsQuery).then((data) => {
@@ -26,7 +25,7 @@ const Skills = () => {
 
 	return (
 		<>
-			<h2 className="head-text">Skills and Experience</h2>
+			<h2 className="head-text">Skills & Experiences</h2>
 
 			<div className="app__skills-container">
 				<motion.div className="app__skills-list">
@@ -47,9 +46,47 @@ const Skills = () => {
 						</motion.div>
 					))}
 				</motion.div>
+				<div className="app__skills-exp">
+					{experiences.map((experience) => (
+						<motion.div className="app__skills-exp-item" key={experience.year}>
+							<div className="app__skills-exp-year">
+								<p className="bold-text">{experience.year}</p>
+							</div>
+							<motion.div className="app__skills-exp-works">
+								{experience.works.map((work) => (
+									<>
+										<motion.div
+											whileInView={{ opacity: [0, 1] }}
+											transition={{ duration: 0.5 }}
+											className="app__skills-exp-work"
+											data-tip
+											data-for={work.name}
+											key={work.name}
+										>
+											<h4 className="bold-text">{work.name}</h4>
+											<p className="p-text">{work.company}</p>
+										</motion.div>
+										<ReactTooltip
+											id={work.name}
+											effect="solid"
+											arrowColor="#fff"
+											className="skills-tooltip"
+										>
+											{work.desc}
+										</ReactTooltip>
+									</>
+								))}
+							</motion.div>
+						</motion.div>
+					))}
+				</div>
 			</div>
 		</>
 	);
 };
 
-export default Skills;
+export default AppWrap(
+	MotionWrap(Skills, "app__skills"),
+	"skills",
+	"app__whitebg"
+);
