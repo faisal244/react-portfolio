782fdd86bad68486de2d4e2ceed94a9959d09ca8
Faisal Mohammed <m.faisal244@gmail.com>
2022-08-25 02:56:40 +0100

    Created Contact form in the footer section
    - User is presented with the choice to email me or to call me - both options open up the users mail app or gives them a prompt to confirm calling me on my provided mobile number
    
    - Contact form submissions go to the sanity cms backend, into an inbox for me to read later
    
    - Thank you message is displayed to the user once the form has been submitted

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,9 +1,112 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { images } from "../../constants";
+import { AppWrap, MotionWrap } from "../../wrapper";
+import { client } from "../../client";
 
 import "./Footer.scss";
 
 const Footer = () => {
-	return <div>Footer</div>;
+	const [formData, setFormData] = useState({
+		name: "",
+		email: "",
+		message: "",
+	});
+	const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+	const [loading, setLoading] = useState(false);
+
+	const { name, email, message } = formData;
+
+	const handleChangeInput = (event) => {
+		const { name, value } = event.target;
+
+		setFormData({ ...formData, [name]: value });
+	};
+
+	const handleSubmit = () => {
+		setLoading(true);
+
+		const contact = {
+			_type: "contact",
+			name: name,
+			email: email,
+			message: message,
+		};
+
+		client
+			.create(contact)
+			.then(() => {
+				setLoading(false);
+				setIsFormSubmitted(true);
+			})
+			.catch((err) => console.log(err));
+	};
+
+	return (
+		<>
+			<h2 className="head-text">Have a coffee & chat with me</h2>
+			<div className="app__footer-cards">
+				<div className="app__footer-card">
+					<img src={images.email} alt="email" />
+					<a href="mailto:m.faisal244@gmail.com" className="p-text">
+						m.faisal244@gmail.com
+					</a>
+				</div>
+
+				<div className="app__footer-card">
+					<img src={images.mobile} alt="mobile" />
+					<a href="tel: +44 07702021386" className="p-text">
+						07702021386
+					</a>
+				</div>
+			</div>
+
+			{!isFormSubmitted ? (
+				<div className="app__footer-form app__flex">
+					<div className="app__flex">
+						<input
+							className="p-text"
+							type="text"
+							placeholder="Your Name"
+							name="name"
+							value={name}
+							onChange={handleChangeInput}
+						/>
+					</div>
+					<div className="app__flex">
+						<input
+							className="p-text"
+							type="email"
+							placeholder="Your Email"
+							name="email"
+							value={email}
+							onChange={handleChangeInput}
+						/>
+					</div>
+					<div>
+						<textarea
+							className="p-text"
+							placeholder="Your Message"
+							value={message}
+							name="message"
+							onChange={handleChangeInput}
+						/>
+					</div>
+					<button type="button" className="p-text" onClick={handleSubmit}>
+						{!loading ? "Send Message" : "Sending..."}
+					</button>
+				</div>
+			) : (
+				<div>
+					<h3 className="head-text">Thank you for getting in touch!</h3>
+				</div>
+			)}
+		</>
+	);
 };
 
-export default Footer;
+export default AppWrap(
+	MotionWrap(Footer, "app__footer"),
+	"contact",
+	"app__whitebg"
+);
